b933843e72eb6a64d3f3ceafb4c3a7b81c0da38c
Iztok Jeras <iztok.jeras@gmail.com>
2026-06-17 01:38:25 +0200

    bugreport: testcase for issue #10622, smaller floorplan, increased slew/cap margins

--- test/flow.tcl
+++ test/flow.tcl
@@ -82,10 +82,12 @@ set_dont_use $dont_use
 
 estimate_parasitics -placement
 
+report_layer_rc
+
 buffer_ports -inputs
 buffer_ports -outputs
 
-repair_design -slew_margin $slew_margin -cap_margin $cap_margin
+repair_design -slew_margin $slew_margin -cap_margin $cap_margin -verbose
 
 repair_tie_fanout -separation $tie_separation $tielo_port
 repair_tie_fanout -separation $tie_separation $tiehi_port
--- test/spm_sky130hd.tcl
+++ test/spm_sky130hd.tcl
@@ -7,8 +7,12 @@ set synth_verilog "spm_sky130hd.v"
 set design "spm"
 set top_module "spm"
 set sdc_file "spm_sky130hd.sdc"
-set die_area {0 0 299.96 300.128}
-set core_area {9.996 10.08 289.964 290.048}
+set die_area {0 0 91.425 102.145}
+set core_area {5.520 10.880 85.560 89.760}
+set global_place_density 0.45
+set global_place_pad 0
+set slew_margin 20
+set cap_margin 20
 
 set_debug_level RSZ resizer 10
 
